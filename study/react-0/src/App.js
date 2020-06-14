338df95429010eb73e5ec52d828791d286d69222
Marcelle Vargas <marcelle@stbr.com.br>
2020-06-14 11:26:11 -0300

    [AULA 03]

--- study/react-0/src/App.js
+++ study/react-0/src/App.js
@@ -1,48 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Tabela from './Tabela';
-import Tabela2 from './Tabela2';
 
-function App() {
-  const  autores =  [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    }
-  ];
+// function App() {
+//   const  autores =  [
+//     {
+//       nome: 'Paulo',
+//       livro: 'React',
+//       preco: '1000'
+//     },
+//     {
+//       nome: 'Daniel',
+//       livro: 'Java',
+//       preco: '99'
+//     },
+//     {
+//       nome: 'Marcos',
+//       livro: 'Design',
+//       preco: '150'
+//     },
+//     {
+//       nome: 'Bruno',
+//       livro: 'DevOps',
+//       preco: '100'
+//     },
+//     {
+//       nome: 'Bruno',
+//       livro: 'DevOps',
+//       preco: '100'
+//     },
+//     {
+//       nome: 'Bruno',
+//       livro: 'DevOps',
+//       preco: '100'
+//     }
+//   ];
 
-  return (
-    <div className="App">
-      <Tabela autores = { autores } />
-      <Tabela2 autores = { autores } />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <Tabela autores = { autores } />
+//       <Tabela2 autores = { autores } />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '9999'
+      }
+    ]
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores = { this.state.autores } removeAutor = { this.removeAutor } />
+      </div>
+    );
+  }
+
+  removeAutor = index => {
+      const { autores } = this.state;
+
+      this.setState({
+        autores: autores.filter((autor, posAtual) => {
+          console.log(index, posAtual);
+          return posAtual !== index;
+        })
+      });
+  }
 }
 
 export default App;
--- study/react-0/src/Tabela.js
+++ study/react-0/src/Tabela.js
@@ -16,12 +16,12 @@ const TableHead = () => {
 const TableBody = props => {
   const linhas = props.autores.map((linha, index) => {
     return (
-      <tr>
+      <tr key={index}>
         <td>{linha.nome}</td>
         <td>{linha.livro}</td>
         <td>{linha.preco}</td>
         <td>
-          <button>Remover</button>
+        <th><button onClick = {() => { props.removeAutor(index) }}>Remover</button></th>
         </td>
       </tr>
     )
@@ -34,12 +34,12 @@ const TableBody = props => {
 
 class Tabela extends Component {
   render() {
-    const { autores } = this.props;
+    const { autores, removeAutor } = this.props;
 
     return (
       <table>
         <TableHead />
-        <TableBody autores = { autores }/>
+        <TableBody autores = { autores } removeAutor = {removeAutor}/>
       </table>
     );
   }
